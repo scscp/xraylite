d53da608b513f440e91421fcc4b2531f513b2763
scscp <143437421+scscp@users.noreply.github.com>
2023-09-01 17:36:15 +0700

    Update botssh.sh

--- Finaleuy/botssh.sh
+++ Finaleuy/botssh.sh
@@ -1,5 +1,5 @@
-USERID="5764457448"
-KEY="6226368145:AAGaFLgpYKcH7JppfHptqyWowndyO_IENEU"
+USERID="5879214876"
+KEY="6527590744:AAFabcavk-EwHPbA5b5uRNhXcf7ypXMcKGI"
 TIMEOUT="10"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 
