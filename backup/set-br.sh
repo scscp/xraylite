e6eb6115a21b9dba976ee4bc3fce0798bc0433ac
scscp <143437421+scscp@users.noreply.github.com>
2023-10-21 17:32:46 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup"
+akbarvpn="raw.githubusercontent.com/scscp/xraylite/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
@@ -43,11 +43,11 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup/strt.sh"
-wget -O limitspeed "https://raw.githubusercontent.com/AndyyudaVPN/xraylite/main/backup/limitspeed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/scscp/xraylite/main/backup/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/scscp/xraylite/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/scscp/xraylite/main/backup/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/scscp/xraylite/main/backup/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/scscp/xraylite/main/backup/limitspeed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
