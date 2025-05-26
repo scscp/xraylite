d4645eb8c33a99adab14c8196c42bb361ded20a4
scscp <143437421+scscp@users.noreply.github.com>
2025-05-26 10:30:22 -0700

    Add files via upload

---
+++ alamat.sh
@@ -0,0 +1,109 @@
+1. Melati No.12, Cibadak, Kec. Sukabumi, Kab. Sukabumi, Jabar, 43151
+2. Mawar No.45, Cisarua, Kec. Cimahi Selatan, Kota Cimahi, Jabar, 40531
+3. Kenanga No.89, Cibiru, Kec. Cileunyi, Kab. Bandung, Jabar, 40393
+4. Anggrek No.23, Bojongloa, Kec. Bojongloa Kidul, Kota Bandung, Jabar, 40234
+5. Flamboyan No.77, Pasir Jaya, Kec. Cibiru, Kab. Bandung, Jabar, 40614
+6. Dahlia No.11, Sukaluyu, Kec. Cipatat, Kab. Bandung Barat, Jabar, 40554
+7. Cempaka No.18, Karanganyar, Kec. Lembang, Kab. Bandung Barat, Jabar, 40391
+8. Teratai No.34, Sukamaju, Kec. Cidadap, Kota Bandung, Jabar, 40143
+9. Seroja No.50, Rancamanyar, Kec. Bojongsoang, Kab. Bandung, Jabar, 40389
+10. Cemara No.22, Sukamurni, Kec. Cimahi Tengah, Kota Cimahi, Jabar, 40521
+
+11. Melati No.9, Sukaresmi, Kec. Cigugur, Kab. Kuningan, Jabar, 45561
+12. Mawar No.54, Karangsari, Kec. Cireunghas, Kab. Sukabumi, Jabar, 43155
+13. Kenanga No.31, Cibeber, Kec. Bayongbong, Kab. Garut, Jabar, 44151
+14. Anggrek No.29, Sukaluyu, Kec. Sukajadi, Kota Bandung, Jabar, 40161
+15. Flamboyan No.68, Margahayu Selatan, Kec. Bekasi Selatan, Kota Bekasi, Jabar, 17141
+16. Dahlia No.12, Sukajadi, Kec. Sukaraja, Kab. Bogor, Jabar, 16712
+17. Cempaka No.14, Cibinong, Kec. Cibinong, Kab. Bogor, Jabar, 16914
+18. Teratai No.83, Sukajaya, Kec. Sukakarya, Kab. Bekasi, Jabar, 17510
+19. Seroja No.12, Bojong Kulur, Kec. Babakan Madang, Kab. Bogor, Jabar, 16810
+20. Cemara No.66, Cimenteng, Kec. Cibeureum, Kota Tasikmalaya, Jabar, 46115
+
+21. Melati No.90, Cikadut, Kec. Buahbatu, Kota Bandung, Jabar, 40286
+22. Mawar No.23, Cikupa, Kec. Serang Baru, Kab. Bekasi, Jabar, 17520
+23. Kenanga No.71, Palasari, Kec. Cihampelas, Kab. Bandung Barat, Jabar, 40562
+24. Anggrek No.56, Linggajaya, Kec. Cikalongwetan, Kab. Bandung Barat, Jabar, 40376
+25. Flamboyan No.85, Setiamanah, Kec. Cisitu, Kab. Bandung, Jabar, 40553
+26. Dahlia No.69, Pameungpeuk, Kec. Pameungpeuk, Kab. Bandung, Jabar, 40382
+27. Cempaka No.19, Kadipaten, Kec. Kadipaten, Kab. Tasikmalaya, Jabar, 46468
+28. Teratai No.11, Cikuda, Kec. Cikuda, Kab. Bogor, Jabar, 16917
+29. Seroja No.42, Sukanagara, Kec. Sukaresmi, Kab. Cianjur, Jabar, 43217
+30. Cemara No.75, Cikembar, Kec. Cikembar, Kab. Sukabumi, Jabar, 43354
+
+31. Melati No.33, Sukasari, Kec. Sukajadi, Kota Bandung, Jabar, 40161
+32. Mawar No.17, Cikalong, Kec. Cikalong Wetan, Kab. Bandung Barat, Jabar, 40376
+33. Kenanga No.41, Sukajaya, Kec. Sukajaya, Kab. Bogor, Jabar, 16710
+34. Anggrek No.55, Kopo, Kec. Bojongloa Kidul, Kota Bandung, Jabar, 40234
+35. Flamboyan No.64, Cililin, Kec. Cililin, Kab. Bandung Barat, Jabar, 40555
+36. Dahlia No.48, Babakan, Kec. Babakan Madang, Kab. Bogor, Jabar, 16810
+37. Cempaka No.26, Sukanagara, Kec. Cisarua, Kab. Bandung Barat, Jabar, 40551
+38. Teratai No.70, Sukamanah, Kec. Cisaat, Kab. Sukabumi, Jabar, 43152
+39. Seroja No.80, Sukaratu, Kec. Cimenyan, Kab. Bandung, Jabar, 40391
+40. Cemara No.44, Sindangsari, Kec. Cisarua, Kab. Bandung Barat, Jabar, 40551
+
+41. Melati No.65, Sukahurip, Kec. Margaasih, Kab. Bandung, Jabar, 40621
+42. Mawar No.20, Karawang Wetan, Kec. Karawang Timur, Kab. Karawang, Jabar, 41316
+43. Kenanga No.38, Parakan, Kec. Parongpong, Kab. Bandung Barat, Jabar, 40652
+44. Anggrek No.10, Sukaluyu, Kec. Cipatat, Kab. Bandung Barat, Jabar, 40554
+45. Flamboyan No.13, Sukamukti, Kec. Cimenyan, Kab. Bandung, Jabar, 40391
+46. Dahlia No.27, Rancamanyar, Kec. Bojongsoang, Kab. Bandung, Jabar, 40389
+47. Cempaka No.35, Pasir Impun, Kec. Cimahi Selatan, Kota Cimahi, Jabar, 40531
+48. Teratai No.58, Sukalarang, Kec. Gunung Putri, Kota Bogor, Jabar, 16124
+49. Seroja No.14, Cipageran, Kec. Cimahi Utara, Kota Cimahi, Jabar, 40512
+50. Cemara No.88, Babakan, Kec. Babakan Madang, Kab. Bogor, Jabar, 16811
+
+51. Melati No.21, Sukabumi, Kec. Cibungbulang, Kab. Bogor, Jabar, 16919
+52. Mawar No.63, Gunung Sindur, Kec. Gunung Sindur, Kab. Bogor, Jabar, 16340
+53. Kenanga No.46, Sindangrasa, Kec. Kertasari, Kab. Bandung, Jabar, 40396
+54. Anggrek No.31, Pasirluyu, Kec. Bogor Selatan, Kota Bogor, Jabar, 16137
+55. Flamboyan No.55, Sukamakmur, Kec. Sukamakmur, Kab. Bogor, Jabar, 16830
+56. Dahlia No.42, Sukahurip, Kec. Margaasih, Kab. Bandung, Jabar, 40621
+57. Cempaka No.79, Cileles, Kec. Jatinangor, Kab. Sumedang, Jabar, 45363
+58. Teratai No.53, Sukagalih, Kec. Parongpong, Kab. Bandung Barat, Jabar, 40614
+59. Seroja No.36, Cileunyi Kulon, Kec. Cileunyi, Kab. Bandung, Jabar, 40393
+60. Cemara No.60, Bojong Lor, Kec. Babelan, Kab. Bekasi, Jabar, 17510
+
+61. Melati No.49, Cimenyan, Kec. Cimenyan, Kab. Bandung, Jabar, 40391
+62. Mawar No.25, Babakan, Kec. Babakan Madang, Kab. Bogor, Jabar, 16811
+63. Kenanga No.69, Sukakarya, Kec. Sukaraja, Kab. Bogor, Jabar, 16712
+64. Anggrek No.15, Rancamahi, Kec. Cibeunying Kidul, Kota Bandung, Jabar, 40124
+65. Flamboyan No.67, Sukasari, Kec. Lemahabang, Kab. Cirebon, Jabar, 45153
+66. Dahlia No.52, Cisalak Pasar, Kec. Cisalak, Kab. Subang, Jabar, 41216
+67. Cempaka No.43, Sukasari, Kec. Cibeunying Kidul, Kota Bandung, Jabar, 40124
+68. Teratai No.36, Cipadung, Kec. Cimahi Utara, Kota Cimahi, Jabar, 40513
+69. Seroja No.24, Pasawahan, Kec. Pasawahan, Kab. Purwakarta, Jabar, 41152
+70. Cemara No.11, Kalijati, Kec. Kalijati, Kab. Subang, Jabar, 41253
+
+71. Melati No.27, Cijerah, Kec. Andir, Kota Bandung, Jabar, 40181
+72. Mawar No.9, Sukajadi, Kec. Sukajadi, Kota Bandung, Jabar, 40161
+73. Kenanga No.50, Bojongloa Kidul, Kec. Bojongloa Kidul, Kota Bandung, Jabar, 40234
+74. Anggrek No.39, Lembang, Kec. Lembang, Kab. Bandung Barat, Jabar, 40391
+75. Flamboyan No.22, Cipedes, Kec. Bandung Wetan, Kota Bandung, Jabar, 40115
+76. Dahlia No.44, Babakan, Kec. Babakan Madang, Kab. Bogor, Jabar, 16810
+77. Cempaka No.31, Sukajaya, Kec. Sukajaya, Kab. Bogor, Jabar, 16710
+78. Teratai No.54, Gunung Putri, Kec. Gunung Putri, Kota Bogor, Jabar, 16124
+79. Seroja No.29, Cimahi Utara, Kec. Cimahi Utara, Kota Cimahi, Jabar, 40513
+80. Cemara No.16, Sukaraja, Kec. Sukaraja, Kab. Bogor, Jabar, 16712
+
+81. Melati No.73, Pasirkaliki, Kec. Cicendo, Kota Bandung, Jabar, 40171
+82. Mawar No.47, Jatinangor, Kec. Jatinangor, Kab. Sumedang, Jabar, 45363
+83. Kenanga No.32, Bojongsoang, Kec. Bojongsoang, Kab. Bandung, Jabar, 40389
+84. Anggrek No.11, Sukamukti, Kec. Cimenyan, Kab. Bandung, Jabar, 40391
+85. Flamboyan No.78, Pameungpeuk, Kec. Pameungpeuk, Kab. Bandung, Jabar, 40382
+86. Dahlia No.26, Ciparay, Kec. Ciparay, Kab. Bandung, Jabar, 40224
+87. Cempaka No.40, Gunung Sari, Kec. Cibogo, Kab. Subang, Jabar, 41252
+88. Teratai No.59, Sukajadi, Kec. Sukajadi, Kota Bandung, Jabar, 40161
+89. Seroja No.68, Banjaran, Kec. Banjaran, Kab. Bandung, Jabar, 40375
+90. Cemara No.24, Cileunyi, Kec. Cileunyi, Kab. Bandung, Jabar, 40393
+
+91. Melati No.15, Bungursari, Kec. Bungursari, Kab. Purwakarta, Jabar, 41113
+92. Mawar No.37, Cikole, Kec. Lembang, Kab. Bandung Barat, Jabar, 40391
+93. Kenanga No.70, Sukamulya, Kec. Cibodas, Kota Cilegon, Jabar, 42414
+94. Anggrek No.49, Cisitu, Kec. Cisitu, Kab. Bandung, Jabar, 40553
+95. Flamboyan No.66, Bayongbong, Kec. Bayongbong, Kab. Garut, Jabar, 44151
+96. Dahlia No.19, Cikupa, Kec. Cikupa, Kab. Tangerang, Jabar, 15710
+97. Cempaka No.28, Cibeureum, Kec. Cibeureum, Kota Tasikmalaya, Jabar, 46115
+98. Teratai No.35, Sukaratu, Kec. Cimenyan, Kab. Bandung, Jabar, 40391
+99. Seroja No.48, Cikadut, Kec. Buahbatu, Kota Bandung, Jabar, 40286
+100. Cemara No.13, Babakan Sari, Kec. Babakan Sari, Kota Bandung, Jabar, 40222
